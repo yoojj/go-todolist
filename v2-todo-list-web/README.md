ffedb7f5705293f6c2f201b650fbd2283d66a97f
유진주 <yoojjxyz@gmail.com>
2023-04-19 18:04:33 +0900

    init

---
+++ v2-todo-list-web/README.md
@@ -0,0 +1,23 @@
+# v2 todo-list front
+
+- svelte 
+
+
+
+
+```bash 
+npm -v
+# 8.19.2 
+
+npm init vite 
+# Project name: v2-todo-list-web  
+# Select a framework: Svelte
+# Select a variant: JavaScript
+
+cd v2-todo-list-web 
+
+npm install 
+
+npm run dev 
+# http://localhost:5173/ 
+``` 
---
+++ v2-todo-list-web/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+
+  console.log('test')
+
+</script>
+
+<style>
+</style>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+
+
+  <Counter />
+
+</main>
